be36ac58cec425469aed60e5621972b1465080f4
TankistPro <nakol.nayk@mail.ru>
2024-12-18 19:35:57 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+'use client';
 import './app.scss';
 
 import { Header } from '@/shared/ui';
 import { CardSwiper } from '@/widgets/card-swiper';
 import { HomeHeader } from '@/widgets/home-header';
+import { useLaunchParams } from '@telegram-apps/sdk-react';
+import { useEffect } from 'react';
 
 export default function Home() {
+	const lp = useLaunchParams();
+
+	useEffect(() => {
+		console.log(lp);
+	}, []);
+
 	return (
 		<div className='page home-page'>
 			<Header>
