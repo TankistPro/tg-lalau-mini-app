9aaeefceab9aee141c3ee8d35bc9690e4452beca
TankistPro <nakol.nayk@mail.ru>
2024-12-18 20:50:47 +0300

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,15 @@
 'use client';
+import { init } from '@/core/init';
 import './app.scss';
 
 import { Header } from '@/shared/ui';
 import { CardSwiper } from '@/widgets/card-swiper';
 import { HomeHeader } from '@/widgets/home-header';
-import { init, initData, viewport } from '@telegram-apps/sdk-react';
 import { useEffect } from 'react';
 
 export default function Home() {
 	useEffect(() => {
 		init();
-
-		console.log(initData);
-
-		(async () => {
-			console.log('requestFullscreen');
-			if (viewport.requestFullscreen.isAvailable()) {
-				await viewport.requestFullscreen();
-				console.log(viewport.isFullscreen()); // true
-			}
-		})();
 	}, []);
 
 	return (
