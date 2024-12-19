ffdd792cafa2d385ffd0ed75964eeffa53e27a8b
TankistPro <nakol.nayk@mail.ru>
2024-12-19 21:51:49 +0300

    test

--- src/shared/ui/header/Header.tsx
+++ src/shared/ui/header/Header.tsx
@@ -5,5 +5,5 @@ import { PropsWithChildren } from 'react';
 type Props = PropsWithChildren;
 
 export function Header({ children }: Props) {
-	return <header style={{ paddingTop: `var(--tg-viewport-content-safe-area-inset-top)` }}>{children}</header>;
+	return <header>{children}</header>;
 }
