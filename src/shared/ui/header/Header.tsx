2298fe327192f8ce58a7507d073dea9223637b69
TankistPro <nakol.nayk@mail.ru>
2024-12-19 21:46:01 +0300

    test

--- src/shared/ui/header/Header.tsx
+++ src/shared/ui/header/Header.tsx
@@ -1,19 +1,9 @@
 'use client';
-import { viewport } from '@telegram-apps/sdk-react';
 import './header.scss';
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren } from 'react';
 
 type Props = PropsWithChildren;
 
 export function Header({ children }: Props) {
-	const { safeAreaInsetTop } = viewport;
-	useEffect(() => {
-		console.log(viewport.safeAreaInsetTop());
-	}, []);
-	return (
-		<header style={{ paddingTop: `${safeAreaInsetTop()}px` }}>
-			{children}
-			{viewport.safeAreaInsetTop()}
-		</header>
-	);
+	return <header style={{ paddingTop: `var(--tg-viewport-content-safe-area-inset-top)` }}>{children}</header>;
 }
